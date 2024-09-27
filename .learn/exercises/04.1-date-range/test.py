3c8bdf99d1fe048114a0c4061185a8ab69969ef0
mariannacast <123494766+mariannacast@users.noreply.github.com>
2024-09-27 04:02:05 +0000

    Pending changes exported from your codespace

--- .learn/exercises/04.1-date-range/test.py
+++ .learn/exercises/04.1-date-range/test.py
@@ -1,35 +1,5 @@
-import pytest, io, os, sys, re
+import pandas as pd
 
-@pytest.mark.it('You must import pandas')
-def test_import_pandas():
-    path = os.path.dirname(os.path.abspath('app.py'))+'/app.py'
-    with open(path, 'r') as content_file:
-        content = content_file.read()
-        regex = re.compile(r"import\s*pandas\s*as\s*pd")
-        assert bool(regex.search(content)) == True
+date_series = pd.date_range(start = '2021-05-01', end = '2021-05-12')
 
-@pytest.mark.it("Use the print function")
-def test_output():
-    path = os.path.dirname(os.path.abspath('app.py'))+'/app.py'
-    with open(path, 'r') as content_file:
-        content = content_file.read()
-        regex = re.compile(r"print\s*\(")
-        assert bool(regex.search(content)) == True
-
-@pytest.mark.it("You should not hard code the expected value")
-def test_hardcoded_output():
-    path = os.path.dirname(os.path.abspath('app.py'))+'/app.py'
-    with open(path, 'r') as content_file:
-        content = content_file.read()
-        regex = re.compile(r"\bDatetimeIndex\s*\(")
-        assert bool(regex.search(content)) == False
-
-@pytest.mark.it('The output should be the expected')
-def test_expected_output(capsys):
-    import app
-    captured = capsys.readouterr()
-    assert   """DatetimeIndex(['2021-05-01', '2021-05-02', '2021-05-03', '2021-05-04',
-               '2021-05-05', '2021-05-06', '2021-05-07', '2021-05-08',
-               '2021-05-09', '2021-05-10', '2021-05-11', '2021-05-12'],
-              dtype='datetime64[ns]', freq='D')
-""" in captured.out
+print(date_series)
